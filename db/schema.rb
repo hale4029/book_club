fa5aa4241267d5380717623ab0be57b706c598b9
Harrison Levin <harrison483@gmail.com>
2019-12-15 13:27:56 -0700

    added author foriegn key to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191215200754) do
+ActiveRecord::Schema.define(version: 20191215202529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 20191215200754) do
     t.string "title"
     t.integer "number_of_pages"
     t.integer "publication_year"
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  add_foreign_key "books", "authors"
 end

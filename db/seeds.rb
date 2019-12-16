77327fdf8e90b8336a71080688e35f5ec89125f9
Harrison Levin <harrison483@gmail.com>
2019-12-15 18:25:43 -0700

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,15 @@ Author.destroy_all
 Book.destroy_all
 
 @author_1 = Author.create(name: "J.K. Rowling")
+@author_2 = Author.create(name: "Ellyn Green")
+@author_3 = Author.create(name: "Harrison Levin")
+
 @book_1 = Book.create(title: "Harry Potter",
                 number_of_pages: 400,
                 publication_year: 2010)
-@book_1.authors << @author_1
-@author_2 = Author.create!(name: "Ellyn Green")
-@author_3 = Author.create!(name: "Harrison Levin")
 @book_2 = Book.create(title: "Harrison's Potter",
                 number_of_pages: 300,
                 publication_year: 2012)
+
 @book_2.authors << [@author_2, @author_3]
+@book_1.authors << [@author_1, @author_3]
